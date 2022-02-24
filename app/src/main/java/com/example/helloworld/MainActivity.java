aaa045fc96e7a7e97a89ddb1ec9c92fc5c6915e8
HUYNHTHIAILINH <huynhthiailinh2105@gmail.com>
2022-02-24 14:47:44 +0700

    ViewModel

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.helloworld.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
     private ActivityMainBinding binding;
+    private MyViewModel model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +20,18 @@ public class MainActivity extends AppCompatActivity {
         View viewRoot = binding.getRoot();
         setContentView(viewRoot);
 
+        model = new ViewModelProvider(this).get(MyViewModel.class);
+        model.getNumber().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                binding.tvCount.setText("" + integer);
+            }
+        });
+
         binding.btnUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int count = Integer.parseInt(binding.tvCount.getText().toString());
-                binding.tvCount.setText("" + ++count);
+                model.increaseNumber();
             }
         });
 
@@ -31,8 +41,7 @@ public class MainActivity extends AppCompatActivity {
     private final View.OnClickListener onClickButtonDown = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            int count = Integer.parseInt(binding.tvCount.getText().toString());
-            binding.tvCount.setText("" + --count);
+            model.decreaseNumber();
         }
     };
 }
